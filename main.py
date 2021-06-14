5d33193de4b38b28857138c861867ec4304b3023
Almyyyy <81880941+Almyyyy@users.noreply.github.com>
2021-06-14 09:04:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,101 +1,126 @@
-import urllib.parse
-import time
-from selenium import webdriver
+from tkinter import scrolledtext
+from tkinter import filedialog
+from tkinter.ttk import Progressbar
+from tkinter import ttk
+from tkinter import *
+from methods import *
 
 
 
-def encode_msg():
-    print('Codifico il messaggio...')
+    # init window #
+window = Tk()
+window.title("Whatsend v1.0 - prerelease") #window name
+window.resizable(False, False) #block window resizing
+tab_control = ttk.Notebook(window) #create tabs inside window
 
-    f = open('messaggio.txt','r')
-    m = f.read()
-    f.close()
+
+
+
+
+
+    # positioning objects #
+
+#init first tab
+tab1 = ttk.Frame(tab_control)
+tab_control.add(tab1, text='Main')
+
+#label
+lbl1 = Label(tab1, text="Select phone numbers file")
+lbl1.grid(column=0, row=0, sticky="w")
+
+#file directory text box
+filedir = Entry(tab1, width=30) #text entry 
+filedir.grid(column=0, row=1) #position of the objects
+
+#choose file button
+def choose_file():
+    window.filename =  filedialog.askopenfilename(title = "Seleziona un file .txt", filetypes = (('text files', 'txt'),))
+    filedir.config(state=NORMAL)
     
-    n = open('encoded.txt','w')
-    for x in range(0, len(m), 1024):
-        buf = m[x:x+1024]
-        z = urllib.parse.quote_plus(buf)
-        n.write(z)
-    n.close()
+    filedir.delete(0, END)
+    filedir.insert(0, window.filename)
+    filedir.config(state=DISABLED)
     
-    return(z)
+
+filebtn = Button(tab1, text="Choose file", width=10, command=choose_file)
+filebtn.grid(column=1, row=1)
+filedir.config(state=DISABLED)
 
 
 
-def gen_link(num, msg):
-    link = 'https://web.whatsapp.com/send?phone=' + num + '&text=' + msg
-    print("     Genero il link...")
-    return link
+#space
+space1 = Label(tab1)
+space1.grid(column=0, row=2)
 
 
 
-def open_link():
-    browser = webdriver.Chrome()
-    browser.get("https://web.whatsapp.com/")
-    input()
+#label
+lbl2 = Label(tab1, text="Write the message")
+lbl2.grid(column=0, row=3, sticky="w")
 
-    msg = encode_msg()
-    
-    with open('numeri.txt') as fp:
-        num = fp.readline()
-        cnt = 1
-        while num:
-            try:
-                num = num.strip()
-                print("Numero {}: {}".format(cnt, num))
-                browser.get(gen_link(num, msg))
-                #browser.save_screenshot("num.png")
-                ctrl = True
+#text area
+txt = scrolledtext.ScrolledText(tab1, width=50, height=3)
+txt.grid(column=0, row=4, columnspan=2)
 
-                while ctrl:
-                    try:     
-                        a = browser.find_element_by_class_name('_3SRfO')
-                        ctrl = False
-                        a = browser.find_element_by_class_name('_1ENRV')
-                        ctrl = True
-                    except:
-                        try:
-                            button = browser.find_element_by_class_name('_1E0Oz')
-                            button.click()
-                            print("     Invio...")
 
-                            while ctrl:
-                                try:
-                                    a = browser.find_element_by_xpath("//span[@data-testid='msg-time']")
-                                except:
-                                    print("     Inviato!")
-                                    ctrl = False
 
-                            time.sleep(30)
-                            ctrl = False
-                        except:
-                            pass
-                        
-            
-                num = fp.readline()
-                cnt += 1
-            except:
-                pass
-                
+#space
+space2 = Label(tab1)
+space2.grid(column=0, row=5)
 
-    browser.close()
+
+
+#start button
+def start():
+    if not filedir.get() or len(txt.get("1.0", "end-1c")) == 0:
+        print("not ok")
+        #startbtn["state"] = "disabled"
+    else:
+        vai(filedir.get(), txt.get("1.0", "end-1c"));
+        print(filedir.get())
+        print(txt.get("1.0", "end-1c"))
+        #startbtn["state"] = "normal"
+
+startbtn = Button(tab1, text="Start", width=10, command=start)
+startbtn.grid(column=0, row=6, columnspan=2)
+
+
+
+#space
+space3 = Label(tab1)
+space3.grid(column=0, row=7)
+
+bar = Progressbar(tab1, length=350)
+bar.grid(column=0, row=8, columnspan=2)
+bar['value'] = 20
 
 
 
 
-
-def send_msg():
-    print("LEGGIMI ATTENTAMENTE!")
-    print("Premendo INVIO si aprirà una scheda di WhatsApp Web.")
-    print("Scannerizza il codice QR dall'account sul quale vuoi azionare il bot.")
-    print("NON CHIUDERE LA SCHEDA MANUALMENTE O LA SESSIONE VERRA' CANCELLATA!")
-    print("Appena hai completato l'accesso a WhatsApp Web, torna nuovamente qui e premi INVIO.")
-
-    input()
-
-    open_link()
-    print("Invio il messaggio...")
+#init second tab
+tab2 = ttk.Frame(tab_control)
+tab_control.add(tab2, text='About')
 
 
 
-send_msg()
+_license = scrolledtext.ScrolledText(tab2, width=50, height=20)
+#txt.configure(background='#f1f1f1')
+_license.insert(INSERT, 
+"""
+MIT License
+Copyright (c) 2021 Giovanni Almirante
+""")
+_license.grid(column=0, row=4, columnspan=2)
+_license.configure(state ='disabled')
+
+
+
+
+
+
+tab_control.pack(expand=1, fill='both')
+
+
+
+    # end of program #
+window.mainloop()
